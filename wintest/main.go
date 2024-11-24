d38c918e8e15f22d5852244fb7bac0c3152dfedb
Paul Miller (AWESOME) <pmiller@microsoft.com>
2024-11-24 12:19:58 -0800

    prove wintest can block

--- wintest/main.go
+++ wintest/main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"context"
 	"log"
+	"net"
 	"os"
 	"os/signal"
+	"strings"
 
 	_ "k8s.io/component-base/logs/json/register"
 	"sigs.k8s.io/kube-network-policies/pkg/networkpolicy"
 	"sigs.k8s.io/kube-network-policies/pkg/windivertinterceptor"
 )
+
+func containsAny(candidate string, tests []string) bool {
+	for _, t := range tests {
+		if strings.Contains(candidate, t) {
+			return true
+		}
+	}
+	return false
+}
 
 // This is a pattern to ensure that deferred functions executes before os.Exit
 func main() {
@@ -18,9 +29,20 @@ func main() {
 		context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
 	log.Println("Started")
+	hosts, err := net.LookupHost("httpbin.org")
+	if err != nil {
+		log.Fatalf("couldn't look up httpbin.org,%s", err)
+	}
+	log.Printf("Blocking %v", hosts)
+
 	windivertinterceptor := windivertinterceptor.New()
-	err := windivertinterceptor.Run(ctx, func(_ context.Context, p networkpolicy.Packet) networkpolicy.Verdict {
+	err = windivertinterceptor.Run(ctx, func(_ context.Context, p networkpolicy.Packet) networkpolicy.Verdict {
 		log.Println(p.ShortString())
+		if containsAny(p.ShortString(), hosts) {
+			log.Println("BLOCKED")
+			return networkpolicy.Drop
+		}
+
 		return networkpolicy.Accept
 	})
 
